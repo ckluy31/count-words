bfb7fe6a8b59d5e534102224111076b46c8153d5
Calvin <calvinluy@gmail.com>
2021-03-17 16:06:28 +1100

    created model for word object

--- src/main/java/com/dgit/demo/models/Word.java
+++ src/main/java/com/dgit/demo/models/Word.java
@@ -1,4 +1,23 @@
 package com.dgit.demo.models;
 
 public class Word {
+
+    private final String word;
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public boolean startsWithLetter(char letter) {
+        // this function returns true if words starts with capitalised or uncapitalised letter
+        return word.charAt(0) == Character.toLowerCase(letter) || word.charAt(0) == Character.toUpperCase(letter);
+    }
+
+    public int checkWordLength() {
+        return word.length();
+    }
 }
