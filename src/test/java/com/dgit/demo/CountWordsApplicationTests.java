21f0a5e75a4e763388fecec73162b2184a55d62a
Calvin <calvinluy@gmail.com>
2021-03-17 15:28:18 +1100

    initial commit

---
+++ src/test/java/com/dgit/demo/CountWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountWordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
